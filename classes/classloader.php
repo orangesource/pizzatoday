e3ca89e515e0417a445c26faacfedd5e61bdd38f
Yannick Berendsen <mryannickz@live.nl>
2014-04-25 21:04:19 +0200

    Changes auto class loader

--- classes/classloader.php
+++ classes/classloader.php
@@ -1,31 +1,4 @@
 <?php
-/*function __classload() {
-	$dir = "classes";
-	$dh  = opendir($dir);
-	while (false !== ($filename = readdir($dh))) {
-		if($filename != "" OR !empty($filename)){
-			$files[] = $filename;
-		}
-	}
-
-	for($i = 0; $i <=count($files); $i++){
-		$newfile = str_replace(array(".", "..", "php"), '', $files);
-		if(!empty($newfile[$i]) OR $newfile[$i] != ""){
-			//echo $newfile[$i];
-			echo $newfile[2].".php";
-			
-			include_once($newfile[$i].".php");
-		}
-	}
-	
-	$form = new checkForm();
-	
-	if($form->checkUsername("asdasd") == true){
-		echo "ja";
-	}
-	
-}*/		
-
 function autoLoadClasses($className) {
     $filename = "classes/" . $className . ".php";
     if (is_readable($filename)) {
@@ -33,8 +6,5 @@ function autoLoadClasses($className) {
     }
 }
  
- 
-spl_autoload_register("autoLoadClasses");
-
-	
+spl_autoload_register("autoLoadClasses");	
 ?>

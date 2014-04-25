c93613023b1b0b20eb06df5c8c7128cbff49c60a
Yannick Berendsen <mryannickz@live.nl>
2014-04-25 14:43:58 +0200

    Pizzatoday, first commit

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);
+?>
+<!doctype html>
+<html>
+<head>
+<link type='text/css' href='css/style.css' rel='stylesheet'>
+<meta charset="utf-8">
+<title>Naamloos document</title>
+</head>
+
+<body>
+<div id='header'>
+	<div id='wrapper_header'>
+    	<div id='header_column'>
+        	a
+        </div>
+    </div>
+</div>
+<div id='nav'>
+	<div id='wrapper_nav'>
+    aaa
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+	include_once("includes/header.php");
+?>
+<div id='wrapper_content'>
+    <div class='frame location'>
+        <div class='infotop'>
+        Dit is een top ding
+        </div>
+        <div class='contentarea'>
+            <p>Vul je woonplaats in</p>
+            <form method='post'>
+                <input type='text' name=''>
+            </form>
+        </div>
+    </div>
+</div>
+<?php 
+	include_once("includes/footer.php");
+?>

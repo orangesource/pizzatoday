0d5c5dbc11335f5c780819e4699ba172faa06c85
Yannick Berendsen <mryannickz@live.nl>
2014-04-26 20:30:58 +0200

    Search on "filiaal" and normal search. Improved style.css

--- classes/siteclass.php
+++ classes/siteclass.php
@@ -28,9 +28,11 @@
 				$title = "StoreLocator";	
 			}
 			
+		
+			
 			if(isset($_POST[$submit])){
 				if(!empty($_POST[$inputname]) && $_POST[$inputname] != ""){
-					header("Location: search.php?s=".$_POST[$inputname]."");
+					header("Location: search.php?c=stores&s=".$_POST[$inputname]."");
 				}else{
 					return "
 					<div class='frame location'>
@@ -38,10 +40,10 @@
 							".$title."
 						</div>
 						<div class='contentarea'>
-							<p><font style='color: red; font-weight: bold;'>U moet een zoekterm opgeven!</font></p>
+							<p><font style='color: red; font-weight: bold;'>U moet een geldige zoekterm opgeven!</font></p>
 							<p>".$information."</p>
 							<form method='post'>
-							<input type='text' name='".$inputname."'><input type='submit' name='".$submit."'>
+							<input type='text' name='".$inputname."'><input type='submit' name='".$submit."' value='ZOEKEN'>
 							</form>
 						</div>
 					</div>
@@ -56,11 +58,78 @@
 					<div class='contentarea'>
 						<p>".$information."</p>
 						<form method='post'>
-						<input type='text' name='".$inputname."'><input type='submit' name='".$submit."'>
+						<input type='text' name='".$inputname."'><input type='submit' name='".$submit."' value='ZOEKEN'>
 						</form>
 					</div>
 				</div>
 			";
+			}
+			
+			
+			
+		}
+		public function search($title, $information, $submit, $inputname, $selectname)
+		{
+			if($information == ""){
+				$information = "";
+			}
+			if($title == ""){
+				$title = "";	
+			}
+			$returnthis = "";
+			$categorien = array("Pizza", "Pasta", "Salades", "Deserts", "Drinks");
+				
+			if(isset($_POST[$submit])){
+				if(!empty($_POST[$inputname]) && $_POST[$inputname] != ""){
+					if(in_array($categorie, $selectname)){
+						header("Location: search.php?s=".$_POST[$inputname]."");
+					}
+				}else{
+					$returnthis .= "
+					<div class='frame location'>
+						<div class='infotop'>
+							".$title."
+						</div>
+						<div class='contentarea'>
+							<p><font style='color: red; font-weight: bold;'>U moet een geldige zoekterm opgeven!</font></p>
+							<p>".$information."</p>
+							<form method='post'>
+							<input type='text' name='".$inputname."'><br/>
+							<select name='".$selectname."'>";
+					$i = 1;
+					foreach($categorien as $data){
+						 $returnthis .= "<option value='".$i++."'>".$data."</option>";
+					}
+					$returnthis .= "</select>
+							<input type='submit' name='".$submit."' value='ZOEKEN'>
+							</form>
+						</div>
+					</div>";
+			return $returnthis;
+					
+				}
+			}else{
+				$returnthis .= "
+					<div class='frame location'>
+						<div class='infotop'>
+							".$title."
+						</div>
+						<div class='contentarea'>
+							<p>".$information."</p>
+							<form method='post'>
+							<input type='text' name='".$inputname."'><br/>
+							<select name='".$selectname."'>";
+					$i = 1;
+					foreach($categorien as $data){
+						 $returnthis .= "<option value='".$i++."'>".$data."</option>";
+					}
+					$returnthis .= "</select>
+							<input type='submit' name='".$submit."' value='ZOEKEN'>
+							</form>
+						</div>
+					</div>";
+					
+			return $returnthis;
 			}
 			
 			
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 	include_once("includes/header.php");
 ?>
 <div id='wrapper_content'>
-    <?php echo $siteFunctions->storeLocator("Zoek een filiaal","Vul hier je woonplaats hier!", "storelocator", "searchtagg");?>
+    <div class='twocolumnarea'><?php echo $siteFunctions->storeLocator("Zoek een filiaal","Vul hier je woonplaats hier!", "storelocator", "searchtagg");?></div>
+    <div class='twocolumnarea'><?php echo $siteFunctions->search("Zoek een product","", "search", "searchname", "categorie");?></div>
+    <div class='clearfix'></div>
 </div>
 <?php 
 	include_once("includes/footer.php");

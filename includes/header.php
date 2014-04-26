817270c5d4329a1a421ae5b719647117a9478b55
Yannick Berendsen <mryannickz@live.nl>
2014-04-26 12:56:36 +0200

    Menu items added

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,10 @@
 <?php
 error_reporting(E_ALL);
+include("classes/classloader.php");
+
+//Laad alle classes in die in de directory classes staan
+
+
 ?>
 <!doctype html>
 <html>
@@ -13,12 +18,27 @@ error_reporting(E_ALL);
 <div id='header'>
 	<div id='wrapper_header'>
     	<div id='header_column'>
-        	a
+        	<?php 
+				autoLoadClasses("checkForm");
+				
+				$form = new checkForm();
+				
+				if($form->checkUsername("Johannie") == true){
+					echo "Ja";	
+				}
+			?>
         </div>
     </div>
 </div>
 <div id='nav'>
 	<div id='wrapper_nav'>
-    aaa
-    </div>
+    	<div class='menuitem menuactive'>HOME</div>
+        <div class='menuitem'>WINKELS</div>
+        <div class='menuitem'>PIZZA'S</div>
+        <div class='menuitem'>OVERIGE PRODUCTEN</div>
+	</div>
 </div>
+
+<?php
+	
+?>
--- index.php
+++ index.php
@@ -1,19 +1,23 @@
-<?php
+<?php 
 	include_once("includes/header.php");
 ?>
 <div id='wrapper_content'>
     <div class='frame location'>
         <div class='infotop'>
-        Dit is een top ding!
+        Dit is een top ding
         </div>
         <div class='contentarea'>
             <p>Vul je woonplaats in</p>
             <form method='post'>
                 <input type='text' name=''>
             </form>
+            
+            <?php
+				
+			?>
         </div>
     </div>
 </div>
-<?php
+<?php 
 	include_once("includes/footer.php");
 ?>

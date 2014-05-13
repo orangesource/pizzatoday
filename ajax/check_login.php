9a4b943acb287347e8310ed377642b0845894cab
dennis eilander <eilander.dennis@gmail.com>
2014-05-13 10:39:00 +0200

    check_login merge changes

--- ajax/check_login.php
+++ ajax/check_login.php
@@ -7,19 +7,18 @@ require "../classes/formclass.php";
 require "../classes/userclass.php";
 require "../classes/sessionclass.php";
 require "../config/config.php";
- 
-//Maak de class connetie 
+
+//Maak de class connetie
 $db = new database();
 $db->startConnect(_HOST, _DB, _USER, _PASS);
 $checkForm = new checkForm($db);
 $checkForm->setUserdata($_POST['email'], $_POST['pass']);
-<<<<<<< HEAD
-$_CUS = new CUS($db); 
-=======
-$sessionClass = new sessionClass($db); 
->>>>>>> 6960a91d2bcfc7916b7411b7f5207f75cbb237b7
+
+$_CUS = new CUS($db);
+
+$sessionClass = new sessionClass($db);
 $userData = new userData($db);
- 
+
 //Error beheer
 $ERROR = array(
 0 => "U bent succesvol aangemeld!",
@@ -29,11 +28,11 @@ $ERROR = array(
 4 => "U heeft geen Password opgegeven!",
 5 => "Het opgegeven Password is niet correct!",
 );
- 
+
 $query = "SELECT * FROM `cms_users`";
 $sth = $db->query($query);
-$data = $sth;		
- 
+$data = $sth;
+
 // echo json_encode(array('success' => false, 'data' => $_POST, 'msg' => $data->email));
 if($checkForm->userNotEmpty() == false){
 	echo json_encode(array('success' => false, 'data' => $_POST, 'msg' => $ERROR[1]));
@@ -47,13 +46,10 @@ if($checkForm->userNotEmpty() == false){
 	echo json_encode(array('success' => false, 'data' => $_POST, 'msg' => $ERROR[5]));
 }else{
 	$userID = $userData->getUserID($_POST['email']);
-<<<<<<< HEAD
 	$_CUS->createCUS($userData);
 	$_CUS->activateSessions($_POST['email']);
-=======
 	$sessionClass->createCUS($userData);
 	$sessionClass->activateSessions($_POST['email']);
->>>>>>> 6960a91d2bcfc7916b7411b7f5207f75cbb237b7
 	echo json_encode(array('success' => true, 'data' => $_POST, 'msg' => $ERROR[0]));
 }
  ?>
